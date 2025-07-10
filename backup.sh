48309b38d5a12f51cda926103495cdb544691c24
Krish Shah <krish.shah@globalpayex.co.in>
2025-07-10 15:44:07 +0530

    refactor: echo messages updated

--- backup.sh
+++ backup.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-echo "Backup script"
+echo "***Backup script***"
 SOURCE=$1
 BACKUP_DIR=$2
 BACKUP_FILENAME="$BACKUP_DIR/backup_$(date +%Y%m%d_%H%M%S).tar.gz"
@@ -14,6 +14,6 @@ if [ -d $SOURCE ]; then
     tar -czf "$BACKUP_FILENAME" -C "$SOURCE" . # destination first, then source. -C and '.' makes it zip only the main directory, and not all the way from the root!!
     echo "Backup complete"
 else
-    echo "Source is not a directory: $SOURCE"
+    echo "Source input is not a directory: $SOURCE"
     exit 1
 fi
